c2e91f86eb8169f062a780387e2c4db67a5ccd6b
MMZidan <mostafa.mzidan@outlook.com>
2024-09-15 20:08:02 +0300

    modified:   task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,12 @@
-USE alx_book_store;
-SHOW COLUMNS from authors; 
+-- USE alx_book_store;
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    DATA_TYPE AS 'Data Type',
+    CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
+    IS_NULLABLE AS 'Is Nullable',
+    COLUMN_DEFAULT AS 'Default Value'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_NAME = 'Books' 
+    AND TABLE_SCHEMA = DATABASE();
